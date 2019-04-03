85e23be58a2aac963466e764828ab302efe2fa5f
epicfrequency <cwanguky@gmail.com>
2019-04-02 23:42:25 -0700

    Update build.sh
    libmad0-dev
    libfaad-dev
    
    Add those two for AAC and MP3 decoding for online radios

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd libupnp-1.6.25
 ./configure
 make install
 
-apt-get -y install build-essential automake autoconf libtool pkg-config libcurl4-openssl-dev intltool libxml2-dev libgtk2.0-dev libnotify-dev libglib2.0-dev libevent-dev dh-make  libboost-dev libmpdclient-dev libsystemd-dev libicu-dev libexpat1-dev libssl-dev libpugixml-dev libavformat-dev libflac-dev libcue-dev libsndfile-dev libid3tag0-dev libsoxr-dev
+apt-get -y install build-essential automake autoconf libtool pkg-config libcurl4-openssl-dev intltool libxml2-dev libgtk2.0-dev libnotify-dev libglib2.0-dev libevent-dev dh-make  libboost-dev libmpdclient-dev libsystemd-dev libicu-dev libexpat1-dev libssl-dev libpugixml-dev libavformat-dev libflac-dev libcue-dev libsndfile-dev libid3tag0-dev libsoxr-dev libfaad-dev libmad0-dev 
 
 cd /root
 
