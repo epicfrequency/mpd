66514e9add8f0da23ddec6a2b8fe412f06e4c716
epicfrequency <cwanguky@gmail.com>
2019-04-17 20:02:55 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 
 systemctl stop mpd
 
+
+
+apt-get -y install build-essential automake autoconf libtool pkg-config libcurl4-openssl-dev intltool libxml2-dev libgtk2.0-dev libnotify-dev libglib2.0-dev libevent-dev dh-make  libboost-dev libmpdclient-dev libsystemd-dev libicu-dev libexpat1-dev libssl-dev libpugixml-dev libavformat-dev libflac-dev libcue-dev libsndfile-dev libid3tag0-dev libsoxr-dev libfaad-dev libmad0-dev 
+
 cd /root
 
 wget https://downloads.sourceforge.net/project/pupnp/pupnp/libUPnP%201.6.25/libupnp-1.6.25.tar.bz2
@@ -9,8 +13,6 @@ cd libupnp-1.6.25
 
 ./configure
 make install
-
-apt-get -y install build-essential automake autoconf libtool pkg-config libcurl4-openssl-dev intltool libxml2-dev libgtk2.0-dev libnotify-dev libglib2.0-dev libevent-dev dh-make  libboost-dev libmpdclient-dev libsystemd-dev libicu-dev libexpat1-dev libssl-dev libpugixml-dev libavformat-dev libflac-dev libcue-dev libsndfile-dev libid3tag0-dev libsoxr-dev libfaad-dev libmad0-dev 
 
 cd /root
 
